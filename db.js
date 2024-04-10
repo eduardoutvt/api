ed88063a6f9c7e7a6027003aa120e478f8147051
eduardoutvt <144716811+eduardoutvt@users.noreply.github.com>
2024-04-10 14:59:11 -0600

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
  
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Corregido: Eliminado el espacio antes de 'root'
-    password: '', // Deja la contraseña vacía si no tienes una configurada
-    database: 'dietadvisordb'
+    host: 'srv1267.hstgr.io',
+    user: 'u875834720_proyecto', // Corregido: Eliminado el espacio antes de 'root'
+    password: 'Milaneso19', // Deja la contraseña vacía si no tienes una configurada
+    database: 'u875834720_dietadvisordb'
 });
 
 connection.connect((err) => {
